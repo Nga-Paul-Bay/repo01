a84c114ca44d352515a50f105012ecff22ff0489
Nganyang Paul Bayendang <bayendang@live.co.za>
2026-01-28 21:08:44 +0200

    Add files via upload

---
+++ run_streamlit.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 27 17:38:34 2026
+
+@author: BBarsch
+"""
+
+import subprocess
+
+#file = "StreamlitDataPipeline2.py"
+
+#file = "app.py"
+#file = "app_plots.py"
+#file = "app_profiler.py"
+file = "app_profiler_menus.py"
+
+
+subprocess.Popen(
+    ["streamlit", "run", file], shell=True
+)
